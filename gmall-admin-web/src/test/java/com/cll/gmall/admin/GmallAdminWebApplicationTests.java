6c7893fec2ed672242516e07fd417e513b08d170
leileichen@galaxyf.com <chenlei239>
2020-03-12 13:09:23 +0800

    电商

---
+++ gmall-admin-web/src/test/java/com/cll/gmall/admin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cll.gmall.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
